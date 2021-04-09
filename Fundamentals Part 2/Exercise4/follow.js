5b58333aae6b7c9442d51b1a04204b24d700bdf8
Sweety Jangale <sweety.jangale@indexnine.com>
2021-04-09 12:35:37 +0530

    Second commit

--- Fundamentals Part 2/Exercise4/follow.js
+++ Fundamentals Part 2/Exercise4/follow.js
@@ -50,9 +50,10 @@ const birthYear = 1948;
 const thisYear = 1965;
 const firstName = "Carlos";
 const lastName = "Stevenson";
+const fullName ="Carlos Stevenson";
+const age=thisYear - birthYear;
 
-
-const greeting = "Hello! My name is " + firstName + " " + lastName + " and I am " + (thisYear - birthYear) + " years old.";
+const greeting = "Hello! My name is " + fullName + " and I am " + age + " years old.";
 
 console.log(greeting);
 
