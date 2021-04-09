21e0a5fc9b86cb343b0ac284a9068450c91ec7df
Sweety Jangale <sweety.jangale@indexnine.com>
2021-04-09 12:24:08 +0530

    First commit

---
+++ Fundamentals Part 2/Exercise1/troubleshooting.js
@@ -0,0 +1,20 @@
+/**
+ * ===== Troubleshooting =====
+ * The function below should log the number 2, however it does not, 
+ * see if you can fix it!
+ * Be sure to fix it in the spirit of the code, do not hard code the result.
+ */
+
+function troubleshooting() {
+	const a = 1;
+	const b = 1;
+
+	// Edit between these lines
+	// =================================
+	return a + b;
+
+	// =================================
+}
+
+// Do not change this 
+module.exports = troubleshooting;
